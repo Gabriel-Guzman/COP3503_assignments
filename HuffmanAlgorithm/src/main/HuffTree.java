ef70b851cc2f62091fc6ef9281850990dea43c98
Gabriel Guzman <guzman.gabriel@outlook.com>
2018-04-23 21:13:27 -0400

    started huffman main script, added useful todo

--- HuffmanAlgorithm/src/main/HuffTree.java
+++ HuffmanAlgorithm/src/main/HuffTree.java
@@ -4,6 +4,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+class Node {
+
+	int data;
+ 	char _char;
+
+ 	Node left;
+ 	Node right;
+}
+
+class _Comparator implements Comparator<Node> {
+ public int compare(Node x, Node y)
+ 	{
+	 	return x.data - y.data;
+ 	}
+}
 
 public class HuffTree implements HuffmanCoding {
 	
@@ -27,22 +46,65 @@ public class HuffTree implements HuffmanCoding {
 				chars[i]++;
 			
 			fr.close();
+			
+			for (int j = 0; j < 255; j++) {
+				if(chars[j] > 0) result += (char) j + " " + chars[j] + "\n";
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("Error reading file.");
 			System.exit(0);
 		}
 		
-		for (int j = 0; j < 255; j++) {
-			
+		return result;
+	}
+	
+	public int[] _getFrequencies(File inputFile) throws FileNotFoundException, Exception {
+		int i;
+		int chars[] = new int[255];
+		
+		
+		FileReader fr = new FileReader(inputFile);
+		
+		
+		// Creating an array the size of the largest extended ascii code.
+		// also the language spec guarantees that it will be initialized to 0
+		
+		
+		// Building the array so that the index = ascii value, and chars[index] is the count of that char
+		while ((i=fr.read()) != -1) {
+			chars[i] = chars[i] + 1;
 		}
 		
-		return null;
+		fr.close();
+		
+		return chars;
+	}
+	
+	private PriorityQueue<Node> _pqFromArray(int[] arr) {
+		LinkedList<Node> tempList = new LinkedList<Node>();
+		PriorityQueue<Node> pq = new PriorityQueue<Node>(255,  new _Comparator());
+		int count = 0;
+		
+		for (int i = 0; i < 255; i++) {
+			if(arr[i] > 0) {
+				count++;
+				Node temp = new Node();
+				temp._char = (char) i;
+				pq.add(temp);
+			}
+		}
+		
+		return pq;
 	}
 
 	@Override
 	public HuffTree buildTree(File inputFile) throws FileNotFoundException, Exception {
-		// TODO Auto-generated method stub
+		int[] frequencies = this._getFrequencies(inputFile);
+		
+		PriorityQueue<Node> pq = _pqFromArray(frequencies);
+		// TODO: Begin build process https://www.geeksforgeeks.org/greedy-algorithms-set-3-huffman-coding/
+		
+	    System.out.println(pq.toString());
 		return null;
 	}
 
--- HuffmanAlgorithm/src/main/Main.java
+++ HuffmanAlgorithm/src/main/Main.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws Exception {
 		
 		File file = new File("C:\\Users\\gabe1\\Documents\\School\\COP3503\\COP3503_assignments\\HuffmanAlgorithm\\src\\input.txt");
 		
 		HuffTree huff = new HuffTree();
-		huff.getFrequencies(file);
+		System.out.println(huff.buildTree(file));
 		
 	}
 }
